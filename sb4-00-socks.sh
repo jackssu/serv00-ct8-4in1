76bab9cbbe4e39946e3476ad156780f7bf54f2df
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-08-09 12:10:53 +0800

    Update sb4-00-socks.sh

--- sb4-00-socks.sh
+++ sb4-00-socks.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export UUID=${UUID:-'5195c04a-552f-4f9e-8bf9-216d257c0839'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
